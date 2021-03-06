852a88a2943faf3bb50286ca4bc3755dfb28cd4e
Ofek Shochat <50481015+OfekShochat@users.noreply.github.com>
2021-03-06 17:17:57 +0200

    Add files via upload

---
+++ delete.py
@@ -0,0 +1,7 @@
+import pika
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.queue_delete(queue='hello')
+channel.queue_delete(queue='job-queue')
+channel.queue_delete(queue='job-queue1')
+channel.queue_delete(queue='job-queue2')
