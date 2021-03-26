6f64a05bbb92eaf214c8017958a41523fb54ff46
OfekShochat <o@shoch.at>
2021-03-26 21:56:17 +0300

    remove unused add_argument

--- serve.py
+++ serve.py
@@ -3,18 +3,21 @@ import json
 import argparse
 
 parser = argparse.ArgumentParser(description="the lc0 testing framework")
-parser.add_argument("--time-control", "-tc", help="time control for the test")
-parser.add_argument("--game-number", "-gn", help="number of tasks to spawn for the test")
-parser.add_argument("--pubkey", "-pk", help="filename to rsa public key")
+parser.add_argument("--time-control", "-tc", help="time control for the test", type=str)
+parser.add_argument("--game-number", "-gn", help="number of tasks to spawn for the test", type=int)
+parser.add_argument("--username", "-un", help="username for credentials", type=str)
+parser.add_argument("--password", "-pw", help="password for credentials", type=str)
+
+args = parser.parse_args()
 
 connection = pika.BlockingConnection(
-    pika.ConnectionParameters(credentials=pika.credentials.PlainCredentials("username", "password"), host='localhost', heartbeat=600, blocked_connection_timeout=500))
+    pika.ConnectionParameters(credentials=pika.credentials.PlainCredentials(args.username, args.password), host='localhost', heartbeat=600, blocked_connection_timeout=500))
 channel = connection.channel()
 
 channel.queue_declare(queue='lc0-jobs', durable=True)
 
-def getEngineDict(name, link, additionalString=""):
-    return {"additionalString":additionalString, "compile":"true", "link":link, "identifier":hash(name) + hash(link), "name":name}
+def getEngineDict(name, link, compile="true", additionalString=""):
+    return {"additionalString":additionalString, "compile":compile, "link":link, "identifier":hash(name) + hash(link), "name":name}
 
 message = {"job":{"tc":"0/10+0.1", "engine1":getEngineDict("lc0-bad", "https://github.com/OfekShochat/lc0"), "engine2":getEngineDict("lc0-regular", "https://github.com/OfekShochat/lc0")}}
 body = json.dumps(message)
