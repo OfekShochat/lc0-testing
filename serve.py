1cde17c343f39845b41f1ea93ad71fbabf10d92a
OfekShochat <o@shoch.at>
2021-03-26 21:37:00 +0300

    authentication, deleting old engines, cleaner logs, results

--- serve.py
+++ serve.py
@@ -1,17 +1,27 @@
-import pika, json
+import pika
+import json
+import argparse
+
+parser = argparse.ArgumentParser(description="the lc0 testing framework")
+parser.add_argument("--time-control", "-tc", help="time control for the test")
+parser.add_argument("--game-number", "-gn", help="number of tasks to spawn for the test")
+parser.add_argument("--pubkey", "-pk", help="filename to rsa public key")
 
 connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
+    pika.ConnectionParameters(credentials=pika.credentials.PlainCredentials("username", "password"), host='localhost', heartbeat=600, blocked_connection_timeout=500))
 channel = connection.channel()
 
-channel.queue_declare(queue='job-queue2', durable=True)
+channel.queue_declare(queue='lc0-jobs', durable=True)
 
-def getEngineDict(name, additionalString=""):
-    return {"additionalString":additionalString, "compile":"true", "link":"https://github.com/OfekShochat/lc0-testing-zip/blob/main/lc0-master.zip?raw=true", "identifier":hash(name), "name":name}
+def getEngineDict(name, link, additionalString=""):
+    return {"additionalString":additionalString, "compile":"true", "link":link, "identifier":hash(name) + hash(link), "name":name}
 
-message = {"job":{"tc":"0/10+0.1", "engine1":getEngineDict("lc0-bad"), "engine2":getEngineDict("lc0-regular")}}
+message = {"job":{"tc":"0/10+0.1", "engine1":getEngineDict("lc0-bad", "https://github.com/OfekShochat/lc0"), "engine2":getEngineDict("lc0-regular", "https://github.com/OfekShochat/lc0")}}
 body = json.dumps(message)
 
-channel.basic_publish(exchange='', routing_key='job-queue2', body=str(body))
+channel.basic_publish(exchange='', routing_key='lc0-jobs', body=str(body),
+                     properties=pika.BasicProperties
+                        (
+                            delivery_mode = 2, # make message persistent
+                        ))
 print(" [x] Sent '%s'" % body)
-connection.close()
--- worker.py
+++ worker.py
@@ -2,15 +2,18 @@ import pika
 import sys
 import os
 import json
-from time import sleep
-from xxhash import xxh32
+from time import sleep, time, ctime
 from random import random
-from subprocess import STDOUT, check_call, CalledProcessError
+from subprocess import STDOUT, check_call, CalledProcessError, check_output
+from shutil import rmtree
+import stat
 
 FNULL = open(os.devnull, 'w')
+PASSWORD = "poopookaki"
+USERNAME = "poop"
 
 def build(engine):
-    os.system("set CC=cl && set CXX=cl && set CC_LD=link && set CXX_LD=link")
+    check_call("set CC=cl && set CXX=cl && set CC_LD=link && set CXX_LD=link", shell=True, stdout=FNULL, stderr=FNULL)
 
     cur_dir = os.getcwd()
     build_opt = json.loads(open("./options.json", "r").read())
@@ -26,44 +29,39 @@ def build(engine):
     cudnn_lib_path = os.path.join(build_opt["cuda_path"], "lib", "x64")
     cudnn_include = os.path.join(build_opt["cuda_path"],"include")
 
-    if build_opt["cudnn"] == "true": os.system("set PATH=%CUDA_PATH%\\bin;%PATH%")
+    if build_opt["cudnn"] == "true": check_call("set PATH=%CUDA_PATH%\\bin;%PATH%", shell=True, stdout=FNULL, stderr=FNULL)
     blas = "true"
     if build_opt["mkl"] == "false" and build_opt["dnnl"] == "false" and build_opt["openblas"] == "false" and build_opt["eigen"] == "false": blas = "false"
 
-    os.chdir("./{}/lc0-master".format(engine["identifier"]))
+    os.chdir("./engines/{}".format(engine["identifier"]))
     try:
-        d = check_call("""meson build --backend {} --buildtype release -Ddx={} -Dcudnn={} -Dplain_cuda={} 
-    -Dopencl={} -Dblas={} -Dmkl={} -Dopenblas={} -Ddnnl={} -Dgtest={} 
-    -Dcudnn_include="{}" -Dcudnn_libdirs="{}" 
-    -Dmkl_include="{}\\include" -Dmkl_libdirs="%MKL_PATH%\\lib\\intel64" -Ddnnl_dir="%DNNL_PATH%"
-    -Dopencl_libdirs="{}" -Dopencl_include="{}" 
-    -Dopenblas_include="{}\\include" -Dopenblas_libdirs="{}\\lib" 
-    -Ddefault_library=static""".format(backend, build_opt["dx12"], build_opt["cudnn"], build_opt["cuda"], 
+        d = check_call("""meson build --backend {} --buildtype release -Ddx={} -Dcudnn={} -Dplain_cuda={} -Dopencl={} -Dblas={} -Dmkl={} -Dopenblas={} -Ddnnl={} -Dgtest=false -Dcudnn_include="{}" -Dcudnn_libdirs="{}" -Dmkl_include="{}\\include" -Dmkl_libdirs="{}\\lib\\intel64" -Ddnnl_dir="{}" -Dopencl_libdirs="{}" -Dopencl_include="{}" -Dopenblas_include="{}\\include" -Dopenblas_libdirs="{}\\lib" -Ddefault_library=static""".format(backend, build_opt["dx12"], build_opt["cudnn"], build_opt["cuda"], 
         build_opt["opencl"], blas, build_opt["mkl"], build_opt["openblas"], 
-        build_opt["dnnl"], "false", cudnn_include, cudnn_lib_path, build_opt["mkl_path"], 
-        build_opt["mkl_path"], build_opt["opencl_lib_path"], 
+        build_opt["dnnl"], cudnn_include, cudnn_lib_path, build_opt["mkl_path"], 
+        build_opt["mkl_path"], build_opt["mkl_path"], build_opt["dnnl_path"], build_opt["opencl_lib_path"], 
         build_opt["cuda_path"], build_opt["openblas_path"], build_opt["openblas_path"]), stdout=FNULL, stderr=FNULL, shell=True)
 
         os.chdir("./build")
-        check_call("msbuild /m /p:Configuration=Release /p:Platform=x64 /p:WholeProgramOptimization=true /p:PreferredToolArchitecture=x64 lc0.sln /filelogger", stderr=STDOUT)
+        check_call("msbuild /m /p:Configuration=Release /p:Platform=x64 /p:WholeProgramOptimization=true /p:PreferredToolArchitecture=x64 lc0.sln /filelogger", stderr=FNULL, stdout=FNULL, shell=True)
 
-    except CalledProcessError:
+    except CalledProcessError as e:
+        print("error encountered:", e)
         print("building {} is not succesful. exiting...".format(engine["name"]))
         exit(1)
 
     os.chdir(cur_dir)
 
 def makecmd(engine):
-    p = os.path.join(str(engine["identifier"]), "lc0-master", "build", "lc0.exe")
-    return p + " -w ./703810.pb.gz"
+    p = os.path.join("engines", str(engine["identifier"]), "build", "lc0.exe")
+    return p + " -w {}".format(os.path.join(os.getcwd(), "703810.pb.gz"))
 
 def cutechess_string(j):
-    cutechess_path = "./cutechess"
+    cutechess_path = "cutechess"
     if os.name == "nt":
-        cutechess_path = os.path.join(cutechess_path, "cutechess-windows.exe")
+        cutechess_path = os.path.join(".", cutechess_path, "cutechess-windows.exe")
     else:
         cutechess_path = os.path.join(cutechess_path, "cutechess-linux")
-    return """{} -engine name={} cmd={} -engine name={} cmd={} -rounds 1 -pgnout out.pgn -bookmode disk -openings file="book.pgn" order=random plies=100 format=pgn -each proto=uci tc={}""".format(
+    return """{} -engine name={} cmd=\"{}\" -engine name={} cmd=\"{}\" -rounds 1 -pgnout out.pgn -bookmode disk -openings file="book.pgn" order=random plies=100 format=pgn -each proto=uci tc={}""".format(
         cutechess_path,
         j["engine1"]["name"], 
         makecmd(j["engine1"]), 
@@ -71,6 +69,13 @@ def cutechess_string(j):
         makecmd(j["engine2"]),
         j["tc"]
     )
+
+def git(link, out):
+    try:
+        check_call("git clone {} ./engines/{} --recurse-submodules".format(link, out))
+        return True
+    except:
+        return False
 
 def download(link, out, unzip=True, verbose=True):
     from requests import get
@@ -93,29 +98,40 @@ def download(link, out, unzip=True, verbose=True):
     else:
         open(out, "wb+").write(c)
 
+def deleteOldEngines():
+    def removeGit(func, path, execinfo):
+        os.chmod(path, stat.S_IWRITE)
+        func(path)
+
+    from glob import glob
+    for i in glob("./engines/**"):
+        modTime = os.path.getmtime(i)
+        if time() - modTime > 18000: # 5 hours
+            rmtree(i, onerror=removeGit)
+            print(" [*] deleted old engine {}".format(i))
+
 def executejob(j):
-    from progress_bar import pbar
+
     print(" [x] downloading")
     print("  - " + j["engine1"]["name"] + "...")
-    download(j["engine1"]["link"], j["engine1"]["identifier"])
+    build1 = git(j["engine1"]["link"], j["engine1"]["identifier"])
     print("  - " + j["engine2"]["name"] + "...")
-    download(j["engine2"]["link"], j["engine2"]["identifier"])
+    build2 = git(j["engine2"]["link"], j["engine2"]["identifier"])
+    print(" [x] building")  
 
-    print(" [x] building")
-    print("  - " + j["engine1"]["name"] + "...")
-    build(j["engine1"])
-    print("  - " + j["engine2"]["name"] + "...")
-    build(j["engine2"])
-
-    print(" [x] generating cutechess string")
-    cutechess_string(j)
-
-def authenticate(j, ch, method, properties):
-    return True
-
-# cutechess-cli.exe -engine name=lc0-regular cmd="C:\Users\User\Downloads\lc02\lc0-master\build\lc0.exe -w C:\Users\User\Downloads\lc0-master\lc0-master\703810.pb.gz 
-# --threads=1 --smart-pruning-factor=0.0 --minibatch-size=1 --max-prefetch=0" -engine name=lc0-confidence-naive cmd="C:\Users\User\Downloads\lc0-master\lc0-master\build\lc0.exe -w C:\Users\User\Downloads\lc0-master\lc0-master\703810.pb.gz --threads=1 --smart-pruning-factor=0.0 --minibatch-size=1 --max-prefetch=0" -each proto=uci tc=inf nodes=1000 
-# -rounds 1 -pgnout C:\Users\User\Downloads\out10.pgn -bookmode disk -openings file="C:\Users\User\Downloads\book_3moves_cp25-49_13580pos (1).pgn" order=random plies=100 format=pgn -concurrency 2
+    if build1: 
+        print("  - " + j["engine1"]["name"] + "...")
+        build(j["engine1"])
+    else:
+        print("  - " + j["engine1"]["name"], "already built")
+    if build2:
+        print("  - " + j["engine2"]["name"] + "...")
+        build(j["engine2"])
+    else:
+        print("  - " + j["engine2"]["name"], "already built")
+    print(cutechess_string(j))
+    print(" [x] starting match")
+    check_output(cutechess_string(j))
 
 def getcutechess():
     if not os.path.isdir("cutechess"): os.mkdir("cutechess")
@@ -123,25 +139,35 @@ def getcutechess():
     download("https://github.com/AndyGrant/OpenBench/blob/master/CoreFiles/cutechess-windows.exe?raw=true", "./cutechess/cutechess-windows.exe", False)
 
 def main():
-
     if not os.path.isdir("cutechess") or not os.path.exists("./cutechess/cutechess-linux") or not os.path.exists("./cutechess/cutechess-windows.exe"):
+        print("no cutechess instelation found.")
+        print("downloading...")
         getcutechess()
 
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', heartbeat=600, blocked_connection_timeout=500))
     channel = connection.channel()
 
-    channel.queue_declare(queue='job-queue', durable=True)
+    channel.queue_declare(queue='lc0-jobs', durable=True)
+
+    def send_results():
+        channel.basic_publish(exchange='', routing_key='lc0-submit', body=open("out.pgn").read(), # json.dumps(response)
+                             properties=pika.BasicProperties(delivery_mode = 2)
+                             ) 
 
     def callback(ch, method, properties, body):
         print(" [x] Received job")
+        st = time()
         j = json.loads(body.decode())
         executejob(j["job"])
+        send_results()
         ch.basic_ack(delivery_tag = method.delivery_tag)
-        print(" [*] Finished")
+        os.remove("out.pgn")
+        print(" [*] Finished in {}s".format(time() - st))
         print(' [*] Waiting for jobs. To exit press CTRL+C')
+        deleteOldEngines()
 
     channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue='job-queue', on_message_callback=callback)
+    channel.basic_consume(queue='lc0-jobs', on_message_callback=callback)
 
     print(' [*] Waiting for jobs. To exit press CTRL+C')
     channel.start_consuming()
